94ba66dc9f8ff0341c99d286fc40d9b4ad5155ea
Adir Makmel <adirmak1@gmail.com>
2020-12-03 20:15:51 +0200

    Init commit

---
+++ WindowsChromeMonitor/WindowsChromeMonitor/main.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    return true;
+}
